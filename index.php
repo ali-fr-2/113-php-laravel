98e9401ed14816d4dd408ad2103208ed05371ac1
ali-fr-2 <alifarjamy2@gmail.com>
2026-06-30 17:31:14 +0330

    126-indexphp

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
+<?php
+
+include "./database/pdo_connection.php";
+$result = $conn->prepare("SELECT * FROM menus");
+$result->execute();
+$menus = $result->fetchAll(PDO::FETCH_ASSOC);
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="fa" dir="rtl">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -26,6 +37,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>وبلاگ | صفحه اصلی</title>
 </head>
+
 <body>
     <div class="modal fade" id="modalSearchBox">
         <div class="modal-dialog modal-dialog-centered">
@@ -45,7 +57,7 @@
             <div class="d-flex align-items-center">
                 <button type="button" class="search-icon" data-bs-toggle="modal" data-bs-target="#modalSearchBox">
                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#fff" class="bi bi-search" viewBox="0 0 16 16">
-                        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+                        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
                     </svg>
                 </button>
                 <button id="switchTheme"></button>
@@ -55,7 +67,7 @@
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar">
                 <i class="fas fa-bars fs-3"></i>
             </button>
-            
+
 
             <div class="collapse navbar-collapse right-nav justify-content-start" id="navbar">
                 <ul class="navbar-nav nav-left">
@@ -65,20 +77,34 @@
                             <span>خانه</span>
                         </a>
                     </li>
-                    <li class="nav-item me-0">
-                        <a class="nav-link mt-3 mt-lg-0" href="/posts.html">
-                            <i class="fas fa-list"></i>
-                            <span>پست ها</span>
-                        </a>
-                    </li>
-                    
+
+
+                    <?php foreach ($menus as $menu) {
+                        if ($menu['parent'] == 0) {  ?>
+                            <li class="nav-item dropdown ">
+                                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    <?= $menu['title']; ?>
+                                </a>
+                                <ul class="dropdown-menu navbar-light bg-light">
+
+                                    <li><a class="dropdown-item" href="#"> <?php foreach ($menus as $item) {
+                                                                                if ($item['parent'] == $menu['id']) {
+                                                                                    echo $item['title'];
+                                                                                }
+                                                                            } ?></a></li>
+                                </ul>
+                            </li>
+                    <?php }
+                    } ?>
+
+
                     <li class="nav-item me-0">
                         <a class="nav-link mt-3 mt-lg-0" href="/login.html">
                             <i class="fa fa-sign-in ms-1"></i>
                             <span>ورود</span>
                         </a>
                     </li>
-                    
+
                     <li class="nav-item me-0">
                         <a class="nav-link mt-3 mt-lg-0" href="/register.html">
                             <i class="fa fa-user-plus ms-1"></i>
@@ -88,7 +114,7 @@
                 </ul>
             </div>
 
-            
+
         </div>
     </nav>
 
@@ -247,7 +273,7 @@
             <p class="fw-bold text-white mb-3 mb-md-0 fs-6">تمامی حقوق برای کدیاد محفوظ می باشد &copy;</p>
             <button type="button" id="scrollUpBtn">
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#fff" class="bi bi-arrow-up-circle" viewBox="0 0 16 16">
-                    <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z"/>
+                    <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z" />
                 </svg>
             </button>
         </div>
@@ -258,4 +284,5 @@
     <script src="js/scrollToUp.js"></script>
     <script src="js/darkMode.js"></script>
 </body>
+
 </html>
