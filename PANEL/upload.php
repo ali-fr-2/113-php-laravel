e608668d915e4894f95f63a684b7ac7e49df8507
ali-fr-2 <alifarjamy2@gmail.com>
2026-07-01 16:32:10 +0330

    131-upload-video

--- PANEL/upload.php
+++ PANEL/upload.php
@@ -1,49 +1,122 @@
 <?php
 
-// if (isset($_POST['submit'])) {
+
+// include "../database/pdo_connection.php";
+
+// if (isset($_POST['submit_image'])) {
 
 //     $tmp = $_FILES['image']['tmp_name'];
-//     $name = $_FILES['image']['name'];
+//     $extension = pathinfo($_FILES['image']['name'], PATHINFO_EXTENSION);
 
-//     if (move_uploaded_file($tmp, "uploads/images/" . $name)) {
+//     $newName = uniqid() . "." . $extension;
+
+//     if (move_uploaded_file($tmp, "uploads/images/" . $newName)) {
 //         echo "آپلود با موفقیت انجام شد.";
+
+//         $result = $conn->prepare("INSERT INTO files SET file_name=?");
+//         $result->bindValue(1, $newName);
+//         $result->execute();
 //     } else {
 //         echo "آپلود ناموفق بود.";
 //     }
-
 // }
+
+// if (isset($_POST['submit_movie'])) {
+
+//     $tmp = $_FILES['movie']['tmp_name'];
+//     $extension = pathinfo($_FILES['movie']['name'], PATHINFO_EXTENSION);
+
+//     $newName = uniqid() . "." . $extension;
+
+//     if (move_uploaded_file($tmp, "uploads/movies/" . $newName)) {
+//         $result = $conn->prepare("INSERT INTO files SET file_name=?");
+//         $result->bindValue(1, $newName);
+//         $result->execute();
+//     } else {
+//         echo "آپلود ناموفق بود.";
+//     }
+// }
+
+
+
+include "../database/pdo_connection.php";
 
 if (isset($_POST['submit'])) {
 
-    $tmp = $_FILES['movie']['tmp_name'];
-    $name = $_FILES['movie']['name'];
+    $type = $_POST['type']; // image یا movie
 
-    if (move_uploaded_file($tmp, "uploads/movies/" . $name)) {
+    $file = $_FILES['file'];
+
+    $tmp = $file['tmp_name'];
+
+    $extension = pathinfo($file['name'], PATHINFO_EXTENSION);
+
+    $newName = uniqid() . "." . $extension;
+
+    $folder = ($type == "image")
+        ? "uploads/images/"
+        : "uploads/movies/";
+
+    if (move_uploaded_file($tmp, $folder . $newName)) {
+
+        $result = $conn->prepare("INSERT INTO files (file_name) VALUES (?)");
+
+        $result->bindValue(1, $newName);
+
+        $result->execute();
+
         echo "آپلود با موفقیت انجام شد.";
+
     } else {
+
         echo "آپلود ناموفق بود.";
+
     }
 
 }
-
 ?>
 
 <!-- <form action="upload.php" method="POST" enctype="multipart/form-data">
 
     <input type="file" name="image">
 
-    <button type="submit" name="submit">
-        Upload
+    <button type="submit" name="submit_image">
+        Upload images
     </button>
 
-</form> -->
+</form>
 
 <form action="upload.php" method="POST" enctype="multipart/form-data">
 
     <input type="file" name="movie">
 
+    <button type="submit" name="submit_movie">
+        Upload movies
+    </button>
+
+</form> -->
+
+
+<form action="upload.php" method="POST" enctype="multipart/form-data">
+
+    <input type="hidden" name="type" value="image">
+
+    <input type="file" name="file">
+
     <button type="submit" name="submit">
-        Upload
+        Upload Image
+    </button>
+
+</form>
+
+<form action="upload.php" method="POST" enctype="multipart/form-data">
+
+    <input type="hidden" name="type" value="movie">
+
+    <input type="file" name="file">
+
+    <button type="submit" name="submit">
+        Upload Movie
     </button>
 
 </form>
