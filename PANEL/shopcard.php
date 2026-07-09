4933bdd10ddeb33d816c751a4900130974875640
ali-fr-2 <alifarjamy2@gmail.com>
2026-07-09 18:56:08 +0330

    151-discount-complete

--- PANEL/shopcard.php
+++ PANEL/shopcard.php
@@ -7,7 +7,7 @@ include "header.php";
 
 $count = 1;
 
-$total=0;
+$total = 0;
 
 $ID = $_SESSION['id'];
 
@@ -18,9 +18,6 @@ $result->bindValue(1, $ID);
 $result->bindValue(2, 0);
 $result->execute();
 $items = $result->fetchAll(PDO::FETCH_ASSOC);
-
-
-
 
 ?>
 
@@ -56,26 +53,48 @@ $items = $result->fetchAll(PDO::FETCH_ASSOC);
           <tbody>
 
 
-            <?php  foreach($items as $item){?>
-            <tr>
-              <th scope="row"><?= $count++;?></th>
-              <td><?= $item['title'];?></td>
-              <td><img src="./uploads/images/<?=$item['image']?>" style="height: 80px;" alt=""></td>
-              <td><?=$item['price'];?></td>
-              <td> <a href="#" class="btn btn-danger"> حدف از سبد</a></td>
-            </tr>
-            <?php $total+=$item['price']; };?>
-
-
-
+            <?php foreach ($items as $item) { ?>
+              <tr>
+                <th scope="row"><?= $count++; ?></th>
+                <td><?= $item['title']; ?></td>
+                <td><img src="./uploads/images/<?= $item['image'] ?>" style="height: 80px;" alt=""></td>
+                <td><?= $item['price']; ?></td>
+                <td> <a href="#" class="btn btn-danger"> حدف از سبد</a></td>
+              </tr>
+            <?php $total += $item['price'];
+            }; ?>
           </tbody>
         </table>
-        <div class="alert alert-dark fs-3"> مجموع مبلغ سبد خرید شما : <span class=" text text-success fs-3"><?=$total;?></span></div>
+
+        <?php
+
+        if (isset($_POST['discount'])) {
+          $code = $_POST['code'];
+          $result = $conn->prepare("SELECT * FROM `discounts` WHERE code=?");
+          $result->bindValue(1,$code);
+          $result->execute();
+          $discounts = $result->fetch(PDO::FETCH_ASSOC);
+
+          if ($discounts) {
+            $numbercode=$discounts['percent'];
+            $total=(100-$numbercode)*($total/100);
+          } else {
+            echo "چنین کد تخفیفی موجود نیست!";
+          }
+        }
+        ?>
+
+
+        <form action="" method="post">
+          <input class="form-control mb-2" type="text" name="code" style="width: 250px;">
+          <input class="btn btn-success mb-2" type="submit" name="discount" value="اعمال کد تخفیف  ">
+        </form>
+        <div class="alert alert-dark fs-3"> مجموع مبلغ سبد خرید شما : <span class=" text text-success fs-3"><?= $total; ?></span></div>
 
         <div>
           <form action="shopping.php" method="post">
             <input class="btn btn-primary" type="submit" name="shopping" value="پرداخت سبد خرید">
-            <input type="hidden" name="total" value="<?=$total ?>" >
+            <input type="hidden" name="total" value="<?= $total ?>">
           </form>
         </div>
 
